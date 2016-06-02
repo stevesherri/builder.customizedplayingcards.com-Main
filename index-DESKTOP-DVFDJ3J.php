e86685d603e993f60d357fa50dc1583309935358
stevesherrin <steve@sherrinentertainment.com>
2016-06-02 07:47:15 -0400

    6.2.16 start

---
+++ index-DESKTOP-DVFDJ3J.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QPC Card Builder -- Home</title>
+
+    <!-- Bootstrap -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,900,900italic|Oswald:400,700|Indie+Flower|Bitter:400,400italic,700|Passion+One:400,700,900|Anton|Libre+Baskerville:400,400italic,700|Crimson+Text:400,600,600italic,700,700italic|Play:400,700|Francois+One|Questrial|Orbitron:400,500,700,900|Dancing+Script:400,700|Sigmar+One|Chewy|Work+Sans:400,600,500,700,800,900|Lobster+Two:400,400italic,700,700italic|Archivo+Black|Bevan|Alfa+Slab+One|Great+Vibes|Tangerine:400,700' rel='stylesheet' type='text/css'>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/js/myscripts.js" rel="stylesheet">
+
+  </head>
+  <body id="home">
+  	<section class="container">
+    
+    	<div class="content row">
+        
+        	<section class="tools col-lg-12">
+            	<div class="navbar">
+                	<ul class="nav navbar-nav">
+                    	<li><a href="#">Add An Image</a></li>
+               			<li class="dropdown">
+                        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Add/Edit Text <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropDownMenu">
+                            	<li class="divider"></li>
+                            	<li><a tabindex="-1" href="">ONE</a></li>
+                                <li><a tabindex="-1" href="">TWO</a></li>
+                                <li><a tabindex="-1" href="">THREE</a></li>
+                            </ul>
+                         </li>
+                    </ul>
+                </div>
+            </section><!-- TOOLS -->
+            
+        	<section class="main col-lg-8">
+            	<h2>MAIN</h2>
+                <p>bsbdf kjbSDF S FBKJSDFB SDFJB KSDF BKJSF BKSDF JBKDSF BSDF JKDSBFKBS DF SJDKFKLSDF BKJDSFKJBLSDF KJDSFBKBDSFLKSDF</p>
+            </section> <!-- MAIN -->
+            
+        </div> <!-- CONTENT -->
+
+   	</section> <!-- CONTAINER -->
+    
+    <section class="container">
+    	<footer class="row">
+        	<nav class="col-lg-12">
+            	<ul class="breadcrumb">
+        			<li><a href="#">One</a></li>
+        			<li><a href="#">Two</a></li>
+        			<li><a href="#">Three</a></li>
+        		</ul><!-- BREADCRUMBS -->
+            </nav><!-- NAV -->
+        </footer><!-- FOOTER -->
+    </section> <!-- CONTAINER -->
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscripts.js"></script>
+   
+  </body>
+</html>
